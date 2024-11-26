38ffd363120877421077a997cf3cef250d7cbe00
Joel Vaz <joelvaz.whitehat@gmail.com>
2024-11-26 12:56:22 +0000

    Server-Side Template Injection write-up

--- app/back/views.py
+++ app/back/views.py
@@ -822,7 +822,8 @@ def security(request):
         [
             "20241011 - Information Leakage",
             "20241101 - SQL Injection",
-            "20241122 - Cryptographic failure", 
+            "20241122 - Cryptographic failure",
+            "20241126 - Server Side Template Injection",
         ]
         }
 
